3946f8a7cac7d38c61509c169b97c9be0c671b3c
Seth Broweleit <hello[@]getsetbro[.]com>
2013-09-01 22:43:50 -0500

    Way better js

--- background.js
+++ background.js
@@ -1,10 +1,4 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	// No tabs or host permissions needed!
-	chrome.tabs.executeScript({
-		code: 'document.getElementById("player-api").style.position="fixed"'
-	});
-	chrome.tabs.executeScript({
-		code: 'document.getElementById("player-api").style.zIndex="999"'
-	});
+    chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
---
+++ content_script.js
@@ -0,0 +1,5 @@
+document.getElementById("player").style.height = "425px";
+var el = document.getElementById("player-api");
+	el.style.position = "fixed";
+	el.style.zIndex = "999";
+	el.style.top = "99";
