872a7479385b84c13d3e4db1ab164c2ce0f1f5e4
Seth Broweleit <hello[@]getsetbro[.]com>
2013-09-03 10:08:54 -0500

    Changed to a page action ext type
    Now the icon only shows up when you are on a youtube page. No need to
    waste real estate when you wont be using the extension.

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  // If the letter 'g' is found in the tab's URL...
+  if (tab.url.indexOf('youtube.com/watch') > -1) {
+    // ... show the page action.
+    chrome.pageAction.show(tabId);
+  }
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+
+function OnActiveChanged( tabId, selectInfo ){
     chrome.tabs.executeScript(null, {file: "content_script.js"});
-});
+};
+chrome.pageAction.onClicked.addListener(OnActiveChanged);
